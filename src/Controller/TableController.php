b11bd294639c373df997c12106f2c9cff78512e4
BIVILLE Allan <biviall@ndlp.fr>
2021-10-19 16:05:33 +0200

    commit

--- src/Controller/TableController.php
+++ src/Controller/TableController.php
@@ -2,22 +2,55 @@
 
 namespace App\Controller;
 
+use App\Form\TableChoiceType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-    /**
-     * @Route("/table", name="table")
-     */
+
+/**
+ * @Route("/table", name="table")
+ */
 class TableController extends AbstractController
 {
     /**
+     * @Route("/select", name="table_select")
+     */
+    public function select(Request $request)
+    {
+        $form = $this->createForm(TableChoiceType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            $data = $form->getData();
+            $n = $data['table_number'];
+            $max = $data['table_max'];
+            $color = $data['table_color'];
+
+            $response = $this->render('table/index.html.twig', [
+                'form' => $form->createView(),
+                'n' => $n,
+                'max' => $max,
+                'color' => $color,
+            ]);
+        } else {
+            $response = $this->render('table/index.html.twig', [
+                'form' => $form->createView(),
+            ]);
+        }
+    }
+    
+    /**
      * @Route("/print/{n}/{max}", name="table_print")
      */
-    public function index(int $n, int $max): Response
+    public function index(int $n, int $max, Request $request): Response
     {
+        $color = $request->get('c');
         return $this->render('table/index.html.twig', [
             'controller_name' => 'TableController',
             'n' => $n,
+            'max' => $max,
+            'color' => $color,
         ]);
     }
 }
---
+++ src/Form/TableChoiceType.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+
+class TableChoiceType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder->add('table_number', ChoiceType::class, [
+            //'attr' => ['style' => 'width:35px'],
+            'choices'  => [
+                'Table de 1' => 1,
+                'Table de 2' => 2,
+                'Table de 3' => 3,
+                'Table de 4' => 4,
+                'Table de 5' => 5,
+                'Table de 6' => 6,
+                'Table de 7' => 7,
+                'Table de 8' => 8,
+                'Table de 9' => 9,
+            ],
+        ])->add('table_max', ChoiceType::class, [
+            //'attr' => ['style' => 'width:35px'],
+            'choices'  => [
+                'Max 1' => 1,
+                'Max 2' => 2,
+                'Max 3' => 3,
+                'Max 4' => 4,
+                'Max 5' => 5,
+                'Max 6' => 6,
+                'Max 7' => 7,
+                'Max 8' => 8,
+                'Max 9' => 9,
+            ],
+        ])->add('table_color', ChoiceType::class, [
+            //'attr' => ['style' => 'width:35px'],
+            'choices'  => [
+                'Couleur rouge' => 'red',
+                'Couleur jaune' => 'yellow',
+                'Couleur orange' => 'orange',
+                'Couleur marron' => 'brown',
+                'Couleur noir' => 'black',
+                'Couleur gris' => 'gray',
+                'Couleur violet' => 'purple',
+                'Couleur pink' => 'pink',
+                'Couleur bleu' => 'blue',
+            ],
+        ])
+        
+        
+        ->add('Choisir', SubmitType::class, [
+            'attr' => ['class' => 'save'],
+        ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
